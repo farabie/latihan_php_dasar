1901be9d39634fdf447d263e962d6aab2be7a95f
Muhammad Farabie <farabie024@gmail.com>
2023-11-18 23:51:19 +0700

    Komentar

---
+++ 35.Komentar/komentar.php
@@ -0,0 +1,14 @@
+<?php 
+
+//Komentar dengan 1 baris
+/*
+    Komentar multi baris, bisa untuk,
+    lebih dari satu baris
+*/
+
+function hello():void {
+    echo "Test" . PHP_EOL;
+}
+
+hello();
+?>
